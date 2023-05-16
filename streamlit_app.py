8466ae89779782a60fb05bf05cbdfbff05a55f98
namratakulkarni60 <133704831+namratakulkarni60@users.noreply.github.com>
2023-05-16 21:12:36 +0530

    Update streamlit_app.py
    added streamlit.header

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 #new section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
